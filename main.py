a6052f27211811ff0efccc61f74013e9bfc11429
jrv1302 <rahulvarmajampana1302@gmail.com>
2026-07-05 02:26:31 +0530

    Add global optimization using Differential Evolution

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from scipy.spatial import cKDTree
+from scipy.optimize import differential_evolution
 
 def curve_points(theta_deg, M, X, n=4000, t_min=6, t_max=60):
     theta = np.radians(theta_deg)
@@ -31,6 +32,15 @@ csv_path = "xy_data.csv"
 
 df = pd.read_csv(csv_path)
 data = df[["x", "y"]].values
-loss = make_loss(data,2000)
+coarse_loss = make_loss(data,2000)
 
-print(loss([30,0.03,55]))
+result = differential_evolution(
+    coarse_loss,
+    ((0,50),(-0.05,0.05),(0,100)),
+    maxiter=300,
+    popsize=40,
+    seed=1,
+    polish=True
+)
+
+print(result.x)
